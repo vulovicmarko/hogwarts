455c1c06962f2369f377a72dbc5f373ad6703214
unknown <m.vulovic@tcu.edu>
2020-05-04 02:42:44 -0500

    artifact

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/HogwartsartifactsonlineApplication.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/HogwartsartifactsonlineApplication.java
@@ -1,13 +1,21 @@
 package edu.tcu.cs.hogwartsartifactsonline;
 
+import edu.tcu.cs.hogwartsartifactsonline.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class HogwartsartifactsonlineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HogwartsartifactsonlineApplication.class, args);
+    }
+    @Bean
+    public IdWorker idWorker(){
+        return new IdWorker(1, 1);
     }
 
 }
---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/domain/Result.java
@@ -0,0 +1,4 @@
+package edu.tcu.cs.hogwartsartifactsonline.domain;
+
+public class Result {
+}
